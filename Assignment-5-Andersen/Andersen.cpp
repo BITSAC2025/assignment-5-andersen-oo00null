eb10f50113baab40f9028d7066fc32a9f14b1e30
oo00null <wbnczwbnfs@163.com>
2025-11-17 21:56:25 +0800

    andersen

--- Assignment-5-Andersen/Andersen.cpp
+++ Assignment-5-Andersen/Andersen.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
     SVF::SVFIRBuilder builder;
     auto pag = builder.build();
     auto consg = new SVF::ConstraintGraph(pag);
-    consg->dump();
+    (void)consg;
 
     Andersen andersen(consg);
 
@@ -34,6 +34,118 @@ int main(int argc, char** argv)
 
 void Andersen::runPointerAnalysis()
 {
-    // TODO: complete this method. Point-to set and worklist are defined in A5Header.h
-    //  The implementation of constraint graph is provided in the SVF library
+    auto pag = SVF::PAG::getPAG();
+    for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Addr))
+    {
+        pts[edge->getSrcID()].insert(edge->getDstID());
+    }
+    bool changed = true;
+    while (changed)
+    {
+        changed = false;
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Phi))
+        {
+            const SVF::PhiStmt *phi = SVF::SVFUtil::cast<SVF::PhiStmt>(edge);
+            unsigned res = phi->getResID();
+            for (const auto opVar : phi->getOpndVars())
+            {
+                unsigned op = opVar->getId();
+                for (auto p : pts[op])
+                {
+                    if (pts[res].insert(p).second)
+                        changed = true;
+                }
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Select))
+        {
+            const SVF::SelectStmt *sel = SVF::SVFUtil::cast<SVF::SelectStmt>(edge);
+            unsigned res = sel->getResID();
+            for (const auto opVar : sel->getOpndVars())
+            {
+                unsigned op = opVar->getId();
+                for (auto p : pts[op])
+                {
+                    if (pts[res].insert(p).second)
+                        changed = true;
+                }
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Copy))
+        {
+            unsigned src = edge->getSrcID();
+            unsigned dst = edge->getDstID();
+            for (auto p : pts[src])
+            {
+                if (pts[dst].insert(p).second)
+                    changed = true;
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Call))
+        {
+            unsigned src = edge->getSrcID();
+            unsigned dst = edge->getDstID();
+            for (auto p : pts[src])
+            {
+                if (pts[dst].insert(p).second)
+                    changed = true;
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Ret))
+        {
+            unsigned src = edge->getSrcID();
+            unsigned dst = edge->getDstID();
+            for (auto p : pts[src])
+            {
+                if (pts[dst].insert(p).second)
+                    changed = true;
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::ThreadFork))
+        {
+            unsigned src = edge->getSrcID();
+            unsigned dst = edge->getDstID();
+            for (auto p : pts[src])
+            {
+                if (pts[dst].insert(p).second)
+                    changed = true;
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::ThreadJoin))
+        {
+            unsigned src = edge->getSrcID();
+            unsigned dst = edge->getDstID();
+            for (auto p : pts[src])
+            {
+                if (pts[dst].insert(p).second)
+                    changed = true;
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Load))
+        {
+            unsigned y = edge->getSrcID();
+            unsigned x = edge->getDstID();
+            for (auto p : pts[y])
+            {
+                for (auto v : pts[p])
+                {
+                    if (pts[x].insert(v).second)
+                        changed = true;
+                }
+            }
+        }
+        for (SVF::PAGEdge *edge : pag->getSVFStmtSet(SVF::PAGEdge::Store))
+        {
+            unsigned y = edge->getSrcID();
+            unsigned x = edge->getDstID();
+            for (auto p : pts[x])
+            {
+                for (auto v : pts[y])
+                {
+                    if (pts[p].insert(v).second)
+                        changed = true;
+                }
+            }
+        }
+    }
 }
