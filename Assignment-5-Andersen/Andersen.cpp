bac4bc05da530f57f3b29e4f4f72cf77dfb0942f
oo00null <wbnczwbnfs@163.com>
2025-11-17 22:35:11 +0800

    2

--- Assignment-5-Andersen/Andersen.cpp
+++ Assignment-5-Andersen/Andersen.cpp
@@ -19,7 +19,6 @@ int main(int argc, char** argv)
     SVF::SVFIRBuilder builder;
     auto pag = builder.build();
     auto consg = new SVF::ConstraintGraph(pag);
-    consg->dump();
     (void)consg;
 
     Andersen andersen(consg);
